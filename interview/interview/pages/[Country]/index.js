27d3ebc5c4c611c22ee21f1d54232b9654f1d20f
sanalms123 <mssanal933@gmail.com>
2023-09-21 16:25:52 +0530

    second commit

--- interview/interview/pages/[Country]/index.js
+++ interview/interview/pages/[Country]/index.js
@@ -1,25 +1,27 @@
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 function Counry() {
-    const router = useRouter()
-    const [Data, setData] = useState()
-    useEffect(() => {
-        axios.get("https://restcountries.com/v3.1/all").then((res) => {
-         const data =  res?.data?.filter((items)=>{
-           return items?.name?.common === router?.query?.Country
-          })
-        setData(data)
-
-        });
-      }, []);
+  const router = useRouter();
+  const [Data, setData] = useState();
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      const data = res?.data?.filter((items) => {
+        return items?.name?.common === router?.query?.Country;
+      });
+      setData(data);
+    });
+  }, []);
   return (
-    <div style={{flexDirection:"column"}} className='d-flex justify-content-center align-items-center'>
-        <h1>{router?.query?.Country}</h1>
-        <img width={300} height={200} src={Data &&Data[0].flags?.png}></img>
+    <div
+      style={{ flexDirection: "column" }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      <h1>{router?.query?.Country}</h1>
+      <img width={300} height={200} src={Data && Data[0].flags?.png}></img>
     </div>
-  )
+  );
 }
 
-export default Counry
+export default Counry;
