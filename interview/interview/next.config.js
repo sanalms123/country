eb8ea1753d293485e9cafb1c20e7f88afc373846
sanalms123 <mssanal933@gmail.com>
2023-09-21 16:20:32 +0530

    first commit

---
+++ interview/interview/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: true,
+  swcMinify: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'flagcdn.com',
+  //       port: '',
+  //       pathname: '/account123/**',
+  //     },
+  //   ],
+  // },
+}
+
+module.exports = nextConfig
